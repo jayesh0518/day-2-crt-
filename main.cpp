15383193354cba1921d5b2f1b41cd5bd788b4f55
jayesh0518 <jaytan1005s@gmail.com>
2026-05-01 15:18:30 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+  
+    //for support 1
+    //for bill 2 
+    // for tech team 3 
+    int choice;
+    cin>> choice ;
+    
+    switch (choice){
+        case 1:
+        cout<<"support is on";
+        break;
+        case 2:
+        cout<<"bill done";
+         break;
+        
+        case 3:
+        cout<<"tech done";
+         break;
+         
+         default:
+         cout<<"invalid";
+    }
+    
+}
